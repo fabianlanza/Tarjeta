e6e0b2a05cd0e69d8d320e02162cbcfe789ed404
Fabian1 <60055343+fabianlanza@users.noreply.github.com>
2024-12-02 22:31:20 -0600

    Tema oscuro terminado

--- index.js
+++ index.js
@@ -4,12 +4,44 @@ const btnDark = document.querySelector('.btn-dark');
 const body = document.querySelector('body');
 const tarjeta = document.querySelector('.tarjeta');
 
+const estadoDark = localStorage.getItem('dark-mode');
 
 function activarModoDark(){
-    tarjeta.classList.toggle('modo-dark-tarjeta');
-    body.classList.toggle('modo-dark-body');
+    tarjeta.classList.add('modo-dark-tarjeta');
+    body.classList.add('modo-dark-body');
+    localStorage.setItem('dark-mode', 'activado');
+}
+
+function desactivarModoDark(){
+    tarjeta.classList.remove('modo-dark-tarjeta');
+    body.classList.remove('modo-dark-body');
+    localStorage.setItem('dark-mode', 'desactivado');
+}
+
+
+// Verificar si el modo dark esta activado al entrar a la pagina
+if(estadoDark === "activado"){
+    activarModoDark();
+    btnDark.textContent="Light";
+}
+else{
+    desactivarModoDark();
+    btnDark.textContent="Dark";
 }
 
 
 
-btnDark.addEventListener('click', activarModoDark);
+
+btnDark.addEventListener('click', () => {
+
+    const estadoDark = localStorage.getItem("dark-mode");
+    if(estadoDark==="desactivado"){
+        activarModoDark();
+        btnDark.textContent="Light";
+    }
+    else{
+        desactivarModoDark();
+        btnDark.textContent="Dark";
+    }
+});
+
